a0d04dc4273264807701465c9bc34fa91bedecab
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-19 14:29:53 -0300

    improve: replace simple button to component with polymorphism

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,20 @@
 import clsx from "clsx";
 import { ReactNode } from "react"
+import { Slot } from "@radix-ui/react-slot";
 
 export interface ButtonProps {
   children: ReactNode | string;
   sizeFull: boolean;
+  asChild: boolean
   execute?(): void
 }
 
-export function Button({ children, sizeFull, execute = () => {} }: ButtonProps) {
+export function Button({ children, sizeFull, execute = () => {}, asChild }: ButtonProps) {
+
+  const Comp = asChild ? Slot : 'button';
+
   return (
-    <button
+    <Comp
       onClick={() => execute()}
       className={
         clsx('bg-purple-500 flex justify-center gap-2 items-center rounded text-white-50 hover:bg-purple-600 p-2 text-xs', {
@@ -19,6 +24,6 @@ export function Button({ children, sizeFull, execute = () => {} }: ButtonProps) 
       }
     >
       {children}
-    </button>
+    </Comp>
   );
 };
